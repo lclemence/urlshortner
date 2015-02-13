a27d4a87499bdad5a77c65a539f8b7ac9c11d4f7
Clemence Lelong <lelongclemence@gmail.com>
2015-02-13 09:20:39 -0500

    add view to sign up / sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Urlshortener::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'urlshortner#index'
+  root to: "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

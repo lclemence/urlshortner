81af4bb0c87a4e45062ab0dffaf02e4941b6ed8c
Clemence Lelong <lelongclemence@gmail.com>
2015-02-14 14:41:42 -0500

    add relation between a user and his url

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :url_shorteners, :dependent => :destroy
 end

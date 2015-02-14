95abda80a38c5fb7c3f80c0fc5685e6b90e70aab
Clemence Lelong <lelongclemence@gmail.com>
2015-02-14 17:29:44 -0500

    add test rspec for model UrlShortener

---
+++ spec/url_shortener_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe UrlShortener, :type => :model do
+  before(:each) do
+    generated_password = Devise.friendly_token.first(8)
+    @user = User.create!(:email => 'test@gmail.com', :password => generated_password)
+    @url = UrlShortener.create!(user_id: @user.id, original_url: "http://google.fr")
+  end
+
+  it "return the url to redirect" do
+    expect(@url.redirection).to eq("http://google.fr")
+  end
+
+  it "should log the click" do
+    @url.redirection
+    expect(@url.used).to eq(1)
+  end
+
+  it "should shorten the original url" do
+    expect(@url.shortened_url).not_to eq('')
+  end
+end

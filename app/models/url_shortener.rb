49c6a6b5451fc54e09ff3d7cfd6e556afc1fbb39
Clemence Lelong <lelongclemence@gmail.com>
2015-02-14 14:41:09 -0500

    before create url calculate the shortened url

--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,2 +1,11 @@
 class UrlShortener < ActiveRecord::Base
+  before_create :shortenurl
+
+  belongs_to :user
+
+  private
+
+  def shortenurl
+    self.shortened_url =  MurmurHash3::V32.str_hash(self.original_url)
+  end
 end
